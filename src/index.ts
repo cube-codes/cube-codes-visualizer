93548d6edf03e1a28aa62e8d753485b02adc12a6
Lukas Lentner <Lukas.Lentner@PerfectPattern.de>
2021-03-16 16:35:42 +0100

    Adapt index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+export * from './Animation/Animation';
+export * from './Animation/BeamAnimation';
+export * from './Animation/MoveAnimation';
+
+export * from './AnimationQueue';
+export * from './CubeCamera';
+export * from './CubeletRealisation';
+export * from './CubeRealisation';
+export * from './CubeSituation';
 export * from './CubeVisualizer';
 export * from './RoundedBoxBufferGeometry';
 export * from './Utils';
